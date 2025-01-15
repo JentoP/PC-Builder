6fc0a56fa6b4345ace1aa02247a387b4df8f865e
Jento <jento.pieters@gmail.com>
2025-01-15 18:18:30 +0100

    V12 Added security functionality

---
+++ src/main/java/be/thomasmore/pcbuilder/config/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package be.thomasmore.pcbuilder.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import javax.sql.DataSource;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager() {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        HandlerMappingIntrospector introspector = new HandlerMappingIntrospector();
+        MvcRequestMatcher.Builder mvcMatcherBuilder =
+                new MvcRequestMatcher.Builder(introspector);
+//        De method http.authorizeHttpRequests(…) heeft als parameter een lambda functie
+        http.authorizeHttpRequests(auth -> auth
+//                Dus: als je een url request die begint met ‘/admin/’ dan moet je ingelogd zijn.
+                .requestMatchers(mvcMatcherBuilder.pattern("/admin/**")).authenticated()
+//                Dus: elke request die niet met ‘/admin’ begint is toegankelijk voor iedereen.
+                .anyRequest().permitAll());
+//        enables login form when admin page is accessed
+        http.formLogin(Customizer.withDefaults());
+
+//        enables h2-console
+        http.csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console()));
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        return http.build();
+    }
+}
--- src/main/java/be/thomasmore/pcbuilder/controllers/HomeController.java
+++ src/main/java/be/thomasmore/pcbuilder/controllers/HomeController.java
@@ -5,19 +5,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+import java.util.logging.Logger;
+
 @Controller
 public class HomeController {
+
+    private static final Logger logger = Logger.getLogger(HomeController.class.getName());
+
     @GetMapping({"/", "/home"})
-    public String Home(Model model) {
+    public String Home(Model model, Principal principal) {
+        final String loginName = principal != null ? principal.getName() : null;
+        logger.info("Login name: " + loginName);
         return "home";
     }
 
     /**
      * Displays the home page for the PC builder.
+     *
      * @return the builder template.
      */
     @RequestMapping("/configurator")
     public String pcBuilderHome() {
         return "configurator";
     }
+
+    @GetMapping("/account")
+    public String account(Principal principal) {
+        final String loginName = principal != null ? principal.getName() : null;
+        logger.info("Login name: " + loginName);
+        return "account";
+    }
+
 }
